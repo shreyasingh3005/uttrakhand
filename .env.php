3ceb79eb5a09e86b4eaf315501fb7112d21a5080
Shreya Singh <amanoffcampus@gmail.com>
2026-08-20 16:37:38 +0530

    Updated project

--- .env.php
+++ .env.php
@@ -20,7 +20,7 @@ return [
     'RATE_LIMIT_LOGIN_WINDOW'  => 900,    // 15 minutes in seconds
     'RATE_LIMIT_API'           => 60,     // max API requests
     'RATE_LIMIT_API_WINDOW'    => 60,     // 1 minute
-    'RATE_LIMIT_BACKOFF_BASE'  => 30,     // base backoff in seconds
+    'RATE_LIMIT_BACKOFF_BASE'  => 00,     // base backoff in seconds
 
     // Session
     'SESSION_LIFETIME'         => 7200,    // 2 hours in seconds
@@ -31,8 +31,4 @@ return [
     'APP_DEBUG'                => false,
     'APP_URL'                  => 'http://paleturquoise-tarsier-877492.hostingersite.com',
 ];
-
-
-
-
 
